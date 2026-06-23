d1f9189fa5e9637e074126e5922ff3649f7d6549
erdemgncr <erdemgncr@users.noreply.github.com>
2026-06-24 02:33:51 +0300

    Update: perf + yükleme ekranı + favicon + UI düzeltmeleri

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1319963465" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3321847425" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

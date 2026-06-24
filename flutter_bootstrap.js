1117288044027a1f1132bc2a31304576a9cc31c0
erdemgncr <erdemgncr@users.noreply.github.com>
2026-06-24 03:16:38 +0300

    Perf: fontlar gömüldü (google_fonts kaldırıldı), katmanlar azaltıldı; eşleştiniz sade beyaz MakeLove logosu

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1765453060" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2580449072" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

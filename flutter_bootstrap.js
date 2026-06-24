53b1e071f61a2b44d4b954d54bf697d7bcbda44f
erdemgncr <erdemgncr@users.noreply.github.com>
2026-06-24 13:27:26 +0300

    Gerçek yerel giriş/kayıt hesapları, profil foto ekleme, boost mavi parlaklık, üzgün boş-deste ikonu

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "213427704" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3511257989" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

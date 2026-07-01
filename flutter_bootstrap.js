e76746590499cabec180ce2d8717e0b56443a19c
erdemgncr <erdemgncr@users.noreply.github.com>
2026-07-02 01:10:43 +0300

    Hard emojiler ekran-dışı sarma (başa sarma belli olmaz, tek katman opaklık=akıcı); odalar kartında son mesaj yerine 'X kişi aktif · canlı' + kırmızı yanıp sönen nokta; Özel Mesajlar gelen kutusu (/live/dms) + odalar & oda app bar'ında 'Özel mesajlar' butonu

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4265191271" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "794739211" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

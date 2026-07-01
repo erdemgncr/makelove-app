2391f116b871b37f837efc766059a14988cbae95
erdemgncr <erdemgncr@users.noreply.github.com>
2026-07-02 00:06:29 +0300

    SEO tam (meta/OG/Twitter/robots/sitemap/JSON-LD), title flicker fix; OSM konum servisi (geolocator+Nominatim, ayarlarda Konumumu Bul→şehir); ikon preload (mağaza glitch); beğeni bildirimi Plus gerektirir dialog

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1891179400" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2544082071" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

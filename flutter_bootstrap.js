3f5f51988ac5fb14b6b6ebb60548b5d3198396c4
erdemgncr <erdemgncr@users.noreply.github.com>
2026-07-02 05:27:39 +0300

    Web perf: giriş+mağaza parçacıkları Transform paint-only + tek katman opaklık (Positioned/saveLayer jank'i bitti) + tam sayı hızlar (sarma sıçraması yok); beğenen fotoğrafları da prefetch

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2069679621" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2621978836" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

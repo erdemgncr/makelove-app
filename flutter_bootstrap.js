ddfa3367f973b64121ee2884ec7062bb92c798b5
erdemgncr <erdemgncrr@gmail.com>
2026-07-10 14:06:57 +0300

    Boş-deste parlamasına kesin koruma (5sn grace + timer iptal)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4120313306" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3461506379" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

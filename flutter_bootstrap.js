69855360190a93e556fe6e6f2b44f45d467e3e0e
erdemgncr <erdemgncrr@gmail.com>
2026-07-10 14:14:29 +0300

    Tutarlılık: mağaza sparkle→kalp, Gold kapısı, plan yazıları, boost 0, bayraklar→kalp

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3461506379" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2262127999" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

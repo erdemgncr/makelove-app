93ddc192716559f7dc57b0372bb000ec83153b85
erdemgncr <erdemgncrr@gmail.com>
2026-07-10 03:47:14 +0300

    FINAL: ikon tree-shake (-1.9MB) + foto limit koruması + engelleme/cüzdan kalıcılığı

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1017394309" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3340493567" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

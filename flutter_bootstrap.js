4184f57329de0f8889d64a3467b402728d505927
erdemgncr <erdemgncrr@gmail.com>
2026-07-10 13:28:43 +0300

    Tek yükleme ekranı: first-frame tetikleyici + deste-hazır splash

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3340493567" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4265842637" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
